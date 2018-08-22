b68e76fe76ef9501a0d8125069ac05f1618d61c3
Johan De Claville Christiansen <johan@safeex.com>
2018-08-22 09:12:56 +0200

    Enable CSP debug

---
+++ include/param_config.h
@@ -0,0 +1,13 @@
+/*
+ * param_config.h
+ *
+ *  Created on: Nov 12, 2016
+ *      Author: johan
+ */
+
+#ifndef INCLUDE_PARAM_CONFIG_H_
+#define INCLUDE_PARAM_CONFIG_H_
+
+#define PARAMID_CSP_DEBUG					50
+
+#endif /* INCLUDE_PARAM_CONFIG_H_ */
---
+++ src/csp_debug_param.c
@@ -0,0 +1,13 @@
+/*
+ * csp_debug_param.c
+ *
+ *  Created on: Aug 20, 2018
+ *      Author: johan
+ */
+
+#include <param/param.h>
+#include <param_config.h>
+
+extern uint8_t csp_debug_level_enabled[7];
+
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CSP_DEBUG,        csp_debug,          PARAM_TYPE_UINT8,  7, sizeof(uint8_t),  PARAM_READONLY_FALSE, NULL, "", csp_debug_level_enabled, NULL);
