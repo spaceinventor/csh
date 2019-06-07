ad4e785608909510b4fd0a2ed5c0aec792f16bcc
Johan de Claville Christiansen <johan@space-inventor.com>
2019-06-07 13:24:31 +0200

    Added samplepoint to caninit.sh

--- caninit.sh
+++ caninit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo ip link set dev can0 down
-sudo ip link set dev can0 up type can bitrate 1000000 restart-ms 100
+sudo ip link set dev can0 up type can bitrate 1000000 restart-ms 100 sample-point 0.875
 sudo ip link set dev can0 txqueuelen 100
 sudo watch -n1 "ip --details --statistics link show can0"
 
