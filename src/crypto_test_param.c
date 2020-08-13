437a45addba34eb25292a2a6dae9fa0bb22e5ad7
Joris Aerts <jorisaerts@gmail.com>
2020-08-13 22:27:39 +0200

    Add params

---
+++ src/crypto_test_param.c
@@ -0,0 +1,17 @@
+#include <param/param.h>
+#include <param_config.h>
+
+
+extern uint8_t _crypto_key_public[16];
+extern uint8_t _crypto_key_secret[16];
+extern uint8_t _crypto_remote_key[16];
+extern uint64_t _crypto_remote_counter;
+extern uint64_t _crypto_fail_auth_count;
+extern uint64_t _crypto_fail_nonce_count;
+
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_KEY_PUBLIC,        crypto_key_public,          PARAM_TYPE_DATA,   16, sizeof(uint8_t), PM_DEBUG, NULL, "", &_crypto_key_public[0], NULL);
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_KEY_SECRET,        crypto_key_secret,          PARAM_TYPE_DATA,   16, sizeof(uint8_t), PM_DEBUG, NULL, "", &_crypto_key_secret[0], NULL);
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_REMOTE_KEY,        crypto_remote_key,          PARAM_TYPE_DATA,   16, sizeof(uint8_t), PM_DEBUG, NULL, "", &_crypto_remote_key[0], NULL);
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_REMOTE_COUNTER,    crypto_remote_counter,      PARAM_TYPE_UINT64, 1, sizeof(uint64_t),           PM_DEBUG, NULL, "", &_crypto_remote_counter, NULL);
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_FAUL_AUTH_COUNT,   crypto_fail_auth_count,     PARAM_TYPE_UINT64, 1, sizeof(uint64_t),           PM_DEBUG, NULL, "", &_crypto_fail_auth_count, NULL);
+const PARAM_DEFINE_STATIC_RAM(PARAMID_CRYPTO_FAUL_NONCE_COUNT,  crypto_fail_nonce_count,    PARAM_TYPE_UINT64, 1, sizeof(uint64_t),           PM_DEBUG, NULL, "", &_crypto_fail_nonce_count, NULL);
---
+++ src/crypto_test_param.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <param/param.h>
+
+/*
+extern const param_t crypto_key_public;
+extern const param_t crypto_key_secret;
+
+extern const param_t crypto_remote_key;
+*/
+extern const param_t crypto_remote_counter;
+extern const param_t crypto_faul_auth_count;
+extern const param_t crypto_faul_nonce_count;
