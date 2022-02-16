08bc72b0eaca95df75447376fb3cc7b05dd198db
Johan de Claville Christiansen <johan@space-inventor.com>
2022-02-16 09:49:55 +0100

    Faster stdbuf monitor

--- src/stdbuf_mon.c
+++ src/stdbuf_mon.c
@@ -60,22 +60,46 @@ static vmem_list_t stdbuf_get_base(int node, int timeout) {
 	return ret;
 }
 
-static int stdbuf_get(uint16_t node, uint32_t base, int from, int to, int timeout) {
+static int stdbuf_get(csp_conn_t * conn, uint32_t base, int from, int to, int timeout) {
 
 	int len = to - from;
 	if (len > 200)
 		len = 200;
 
 	struct csp_cmp_message message;
+	message.type = CSP_CMP_REQUEST;
+	message.code = CSP_CMP_PEEK;
 	message.peek.addr = htobe32(base + from);
 	message.peek.len = len;
 
-	if (csp_cmp_peek(node, timeout, &message) != CSP_ERR_NONE) {
+	csp_packet_t * packet = csp_buffer_get(0);
+	if (packet == NULL)
+		return 0;
+
+	
+
+	/* Copy the request */
+	memcpy(packet->data, &message, 7);
+	packet->length = 7;
+
+	//csp_hex_dump("send", packet->data, packet->length);
+
+	csp_send(conn, packet);
+	packet = NULL;
+
+	packet = csp_read(conn, timeout);
+
+	if (packet == NULL)
+		return 0;
+
+	//csp_hex_dump("recv", packet->data, packet->length);
+
+	if (packet->length < 7) {
+		csp_buffer_free(packet);
 		return 0;
 	}
 
-	int ignore __attribute__((unused)) = write(fileno(stdout), message.peek.data, len);
-
+	int ignore __attribute__((unused)) = write(fileno(stdout), &packet->data[7], len);
 	return len;
 
 }
@@ -126,35 +150,41 @@ static int stdbuf_mon_slash(struct slash *slash) {
 
 	printf("Monitoring stdbuf on node %u, base %x, size %u\n", node, vmem.vaddr, vmem.size);
 
+	csp_conn_t * conn = csp_connect(CSP_PRIO_HIGH, node, CSP_CMP, 0, CSP_SO_NONE);
+	if (conn == NULL)
+		return SLASH_ENOMEM;
+
 	while(1) {
 
-		param_pull_queue(&pull_q, 0, node, 1000);
+		param_pull_queue(&pull_q, 0, node, 100);
 		int in = param_get_uint16(stdbuf_in);
 		int out = param_get_uint16(stdbuf_out);
 
 		int got = 0;
 		if (out < in) {
 			//printf("Get from %u to %u\n", out, in);
-			got = stdbuf_get(node, vmem.vaddr, out, in, 1000);
+			got = stdbuf_get(conn, vmem.vaddr, out, in, 100);
 		} else if (out > in) {
 			//printf("Get from %u to %u\n", out, vmem.size);
-			got = stdbuf_get(node, vmem.vaddr, out, vmem.size, 1000);
+			got = stdbuf_get(conn, vmem.vaddr, out, vmem.size, 100);
 		}
 
 		uint16_t out_push = out + got;
 		out_push %= vmem.size;
-	    param_push_single(stdbuf_out, 0, &out_push, 0, node, 1000, version);
+	    param_push_single(stdbuf_out, 0, &out_push, 0, node, 100, version);
 
 	    if (got > 0) {
 	    	continue;
 	    }
 
 		/* Delay (press enter to exit) */
-		if (slash_wait_interruptible(slash, 1000) != 0) {
+		if (slash_wait_interruptible(slash, 100) != 0) {
 			break;
 		}
 
 	};
+
+	csp_close(conn);
 
 	return SLASH_SUCCESS;
 }
