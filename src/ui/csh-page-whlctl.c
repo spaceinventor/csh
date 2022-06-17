80522752c24f5a5fb1a732f38b9b413a7de69d80
Johan de Claville Christiansen <johan@space-inventor.com>
2022-06-17 22:02:19 +0200

    Fix build

--- src/ui/csh-page-whlctl.c
+++ src/ui/csh-page-whlctl.c
@@ -9,7 +9,7 @@ G_DEFINE_TYPE(CshPageWhlctl, csh_page_whlctl, ADW_TYPE_APPLICATION_WINDOW)
 static void csh_page_whlctl_class_init(CshPageWhlctlClass * klass) {
 	GtkWidgetClass * widget_class = GTK_WIDGET_CLASS(klass);
 
-	gtk_widget_class_set_template_from_resource(widget_class, "/com/spaceinventor/csh/pages/whlctl/csh-page-whlctl.ui");
+	gtk_widget_class_set_template_from_resource(widget_class, "/com/spaceinventor/csh/csh-page-whlctl.ui");
 }
 
 static void csh_page_whlctl_init(CshPageWhlctl * self) {
