64cbae0322486c99d5f7678d87af7b179ee6e881
Johan de Claville Christiansen <johan@space-inventor.com>
2022-06-20 13:25:20 +0200

    Added PCDU checkout page

--- src/ui/csh-page-whlctl.c
+++ src/ui/csh-page-whlctl.c
@@ -1,25 +1,31 @@
 #include "csh-page-whlctl.h"
 #include "si-param-label.h"
 
+#include <param/param.h>
+#include <param/param_client.h>
+
 #include <adwaita.h>
 
 struct _CshPageWhlctl {
 	AdwApplicationWindow parent_instance;
-    GtkSpinButton * pidi;
-    GtkSpinButton * pidp;
 };
 
 G_DEFINE_TYPE(CshPageWhlctl, csh_page_whlctl, ADW_TYPE_APPLICATION_WINDOW)
 
 static void csh_page_whlctl_class_init(CshPageWhlctlClass * klass) {
 	GtkWidgetClass * widget_class = GTK_WIDGET_CLASS(klass);
-    
     gtk_widget_class_set_template_from_resource(widget_class, "/com/spaceinventor/csh/csh-page-whlctl.ui");
-    gtk_widget_class_bind_template_child(widget_class, CshPageWhlctl, pidi);
-    gtk_widget_class_bind_template_child(widget_class, CshPageWhlctl, pidp);
 }
+
+
+static int time_handler(void * object) {
+    param_pull_all(0, 6, PM_TELEM, PM_REMOTE | PM_HWREG, 100, 2);
+    return 1;
+}
+
 
 static void csh_page_whlctl_init(CshPageWhlctl * self) {
     si_param_label_get_type();
 	gtk_widget_init_template(GTK_WIDGET(self));
+    g_timeout_add(100, time_handler, self);
 }
--- src/ui/si-param-label.c
+++ src/ui/si-param-label.c
@@ -10,6 +10,7 @@
 struct _SiParamLabel {
 	GtkWidget parent_instance;
 	int param_id;
+    int param_offset;
     GtkLabel * label;
 };
 
@@ -18,25 +19,20 @@ G_DEFINE_TYPE(SiParamLabel, si_param_label, GTK_TYPE_WIDGET);
 enum {
 	PROP_0,
 	PROP_PARAM_ID,
+    PROP_PARAM_OFFSET,
 	PROP_LAST_PROP,
 };
 static GParamSpec * props[PROP_LAST_PROP];
 
-int si_param_label_get_param_id(SiParamLabel *self) {
-    g_return_val_if_fail (SI_IS_PARAM_LABEL(self), 0);
-    return self->param_id;
-}
-
-void si_param_label_set_param_id(SiParamLabel *self, int param_id) {
-    g_return_if_fail (SI_IS_PARAM_LABEL(self));
-    self->param_id = param_id;
-}
 
 static void si_param_label_get_property(GObject * object, guint property_id, GValue * value, GParamSpec * pspec) {
 	SiParamLabel * self = SI_PARAM_LABEL(object);
 	switch (property_id) {
 		case PROP_PARAM_ID:
-			g_value_set_int(value, si_param_label_get_param_id(self));
+			g_value_set_int(value, self->param_id);
+			break;
+        case PROP_PARAM_OFFSET:
+			g_value_set_int(value, self->param_offset);
 			break;
 		default:
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
@@ -48,7 +44,10 @@ static void si_param_label_set_property(GObject * object, guint property_id, con
     SiParamLabel * self = SI_PARAM_LABEL(object);
 	switch (property_id) {
 		case PROP_PARAM_ID:
-			si_param_label_set_param_id(self, g_value_get_int(value));
+			self->param_id = g_value_get_int(value);
+			break;
+        case PROP_PARAM_OFFSET:
+			self->param_offset = g_value_get_int(value);
 			break;
     	default:
 			G_OBJECT_WARN_INVALID_PROPERTY_ID(object, property_id, pspec);
@@ -72,6 +71,7 @@ static void si_param_label_class_init(SiParamLabelClass * klass) {
     object_class->get_property = si_param_label_get_property;
     object_class->dispose = si_param_label_dispose;
     props[PROP_PARAM_ID] = g_param_spec_int ("param_id", "Parameter id", "The id of the parameter", -1, INT_MAX, -1, G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY);
+    props[PROP_PARAM_OFFSET] = g_param_spec_int ("param_offset", "Parameter array offset", "Array index of the parameter", -1, INT_MAX, -1, G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY);
     g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
 	
     GtkWidgetClass * widget_class = GTK_WIDGET_CLASS(klass);
@@ -86,14 +86,12 @@ static int time_handler(void * object) {
     if (self->param_id == 0)
         return 1;
 
-    param_t * param = param_list_find_id(220, self->param_id);
+    param_t * param = param_list_find_id(6, self->param_id);
     if (param == NULL)
         return 1;
 
-    param_pull_single(param, 0, 0, 220, 100, 2);
-
     char buf[100];
-    param_value_str(param, 0, buf, 100);
+    param_value_str(param, self->param_offset, buf, 100);
 
     gtk_label_set_text(self->label, buf);
     return 1;
@@ -101,5 +99,5 @@ static int time_handler(void * object) {
 
 static void si_param_label_init(SiParamLabel * self) {
 	gtk_widget_init_template(GTK_WIDGET(self));
-    g_timeout_add(1000, time_handler, self);
+    g_timeout_add(100, time_handler, self);
 }
