5230da930470d84c48f014d41f5aff6930018968
Johan de Claville Christiansen <johan@space-inventor.com>
2023-01-09 11:43:54 +0100

    Use libcurl to push data over https

--- src/prometheus.c
+++ src/prometheus.c
@@ -8,6 +8,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
+#include <curl/curl.h>
 #include <pthread.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
@@ -20,63 +21,32 @@ static char prometheus_buf[10*1024*1024] = {0};
 static int prometheus_buf_len = 0;
 static int listen_fd;
 
-static char header[1024] =
-		"HTTP/1.1 200 OK;\r\n"
-		"Content-Type: text/plain;\r\n\r\n";
-
-
 void * prometheus_exporter(void * param) {
 
-	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	CURLcode res;
+ 	curl_global_init(CURL_GLOBAL_DEFAULT);
+ 	CURL *curl = curl_easy_init();
 
-	if (setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int)) < 0)
-    	printf("setsockopt(SO_REUSEADDR) failed\n");
-
-	struct sockaddr_in server_addr = {0};
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(9101);
-
-retry_bind:
-	if (bind(listen_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-		printf("Cannot bind prometheus to port 9101\n");
-		sleep(1);
-		goto retry_bind;
-	} else {
-		printf("Prometheus exporter listening on port 9101\n");
-	}
-
-	listen(listen_fd, 100);
+	curl_easy_setopt(curl, CURLOPT_URL, "https://mainecoon.space-inventor.com:8428/api/v1/import/prometheus");
+	curl_easy_setopt(curl, CURLOPT_VERBOSE, 0L);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1L);
 
 	while(1) {
 
-		struct sockaddr_in client_addr;
-		socklen_t client_addr_len;
-		int conn_fd = accept(listen_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+		usleep(10000);
 
-		char in[1024];
-		int bread = read(conn_fd, in, 1024);
-		if (bread == 0) {
-			close(conn_fd);
+		if (prometheus_buf_len == 0) {
 			continue;
 		}
 
-		if (strncmp(in, "GET /metrics", 12) != 0) {
-			close(conn_fd);
-			continue;
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, prometheus_buf);
+		res = curl_easy_perform(curl);
+		if(res != CURLE_OK) {
+      		fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
 		}
-
-		int written = write(conn_fd, header, strlen(header));
-
-		/* Dump queued data */
-		written += write(conn_fd, prometheus_buf, prometheus_buf_len);
-		//printf("Prometheus sent %d bytes\n", written);
 
 		prometheus_clear();
-
-		shutdown(conn_fd, SHUT_RDWR);
-
-		close(conn_fd);
 
 	}
 
