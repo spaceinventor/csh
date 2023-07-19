fb47e2ce3e1d85e08a33c5c9e5a48b2d734a1d9e
kivkiv12345 <kevincarlsen@live.dk>
2023-07-19 08:08:54 +0200

    SI-406 Added "sleep" slash command

---
+++ src/sleep_slash.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <slash/slash.h>
+
+static int cmd_sleep(struct slash *slash) {
+
+	if (slash->argc != 2) {
+        return SLASH_EUSAGE;
+	}
+
+    slash_wait_interruptible(slash, atoi(slash->argv[1]));
+	return SLASH_SUCCESS;
+}
+slash_command(sleep, cmd_sleep, "<sleep ms>", "Sleep the specified amount of milliseconds");
