9762533bbd642b2f8698879e83ed543ef0cf7ed9
edvard <edvard@space-inventor.com>
2023-09-22 10:55:54 +0200

    added more help text on defaults

--- src/csp_init_cmd.c
+++ src/csp_init_cmd.c
@@ -175,7 +175,7 @@ static int csp_ifadd_kiss_cmd(struct slash *slash) {
     optparse_add_help(parser);
     optparse_add_set(parser, 'p', "promisc", 1, &promisc, "Promiscuous Mode");
     optparse_add_int(parser, 'm', "mask", "NUM", 0, &mask, "Netmask (defaults to 8)");
-    optparse_add_int(parser, 'b', "baud", "NUM", 0, &baud, "Baudrate");
+    optparse_add_int(parser, 'b', "baud", "NUM", 0, &baud, "Baudrate (defaults to 1000000)");
     optparse_add_string(parser, 'u', "uart", "STR", &device, "UART device name (defaults to /dev/ttyUSB0)");
     optparse_add_set(parser, 'd', "default", 1, &dfl, "Set as default");
 
@@ -239,7 +239,7 @@ static int csp_ifadd_can_cmd(struct slash *slash) {
     optparse_add_help(parser);
     optparse_add_set(parser, 'p', "promisc", 1, &promisc, "Promiscous Mode");
     optparse_add_int(parser, 'm', "mask", "NUM", 0, &mask, "Netmask (defaults to 8)");
-    optparse_add_int(parser, 'b', "baud", "NUM", 0, &baud, "Baudrate");
+    optparse_add_int(parser, 'b', "baud", "NUM", 0, &baud, "Baudrate (defaults to 1000000)");
     optparse_add_string(parser, 'c', "can", "STR", &device, "CAN device name (defaults to can0)");
     optparse_add_set(parser, 'd', "default", 1, &dfl, "Set as default");
 
