59dc00a6a4a2b6a09338de3b3a7d80e006564806
edvard <edvard@space-inventor.com>
2023-10-06 11:14:51 +0200

    vts does not use hk data now

--- src/param_sniffer.h
+++ src/param_sniffer.h
@@ -11,7 +11,7 @@
 #include <csp/csp.h>
 
 int param_sniffer_crc(csp_packet_t * packet);
-int param_sniffer_log(void * ctx, param_queue_t *queue, param_t *param, int offset, void *reader, long unsigned int timestamp);
+int param_sniffer_log(void * ctx, param_queue_t *queue, param_t *param, int offset, void *reader, long unsigned int timestamp, unsigned int src_node);
 void param_sniffer_init(int add_logfile, int node);
 
 #endif /* SRC_PARAM_SNIFFER_H_ */
