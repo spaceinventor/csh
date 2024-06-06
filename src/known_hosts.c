a8dd3dab84931bd2db3016d186bbbe39bd04b2fe
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2024-06-06 08:59:43 +0200

    Reintroduce "node" & "timeout" CSH slash commands

--- src/known_hosts.c
+++ src/known_hosts.c
@@ -182,3 +182,37 @@ static int cmd_hosts_add(struct slash *slash)
 }
 
 slash_command_sub(node, add, cmd_hosts_add, NULL, NULL);
+
+
+static int cmd_node(struct slash *slash) {
+
+	if (slash->argc < 1) {
+		/* Only print node when explicitly asked,
+			as it should be shown in the prompt.
+			(it may be truncated when the hostname is too long) */
+		printf("Default node = %d\n", slash_dfl_node);
+	}
+
+	if (slash->argc == 2) {
+
+		slash_dfl_node = known_hosts_get_node(slash->argv[1]);
+		if (slash_dfl_node == 0)
+			slash_dfl_node = atoi(slash->argv[1]);
+	}
+
+	return SLASH_SUCCESS;
+}
+slash_command(node, cmd_node, "[node]", "Set global default node");
+
+
+static int cmd_timeout(struct slash *slash) {
+
+	if (slash->argc == 2) {
+		slash_dfl_timeout = atoi(slash->argv[1]);
+	}
+
+    printf("timeout = %d\n", slash_dfl_timeout);
+
+	return SLASH_SUCCESS;
+}
+slash_command(timeout, cmd_timeout, "[timeout ms]", "Set global default timeout");
