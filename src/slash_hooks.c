e8c48b0dce39511cab0ba69690653d35954f063d
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2025-07-11 13:59:31 +0200

    Guard Python specific code to allow building without Python, for example, on Ubuntu 20.04

--- src/slash_hooks.c
+++ src/slash_hooks.c
@@ -1,13 +1,18 @@
 
 #include "loki.h"
+#ifdef HAVE_PYTHON
 #include "python/python_loader.h"
-
+#endif
 
 void slash_on_execute_hook(const char *line) {
 	on_loki_slash_execute_hook(line);
+#ifdef HAVE_PYTHON
     on_python_slash_execute_pre_hook(line);
+#endif
 }
 
 void slash_on_execute_post_hook(const char *line, struct slash_command *command) {
+#ifdef HAVE_PYTHON
     on_python_slash_execute_post_hook(line, command);
+#endif
 }
