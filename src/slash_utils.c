c3e2b8171afd6361034775e4845a0ab4112a7a72
Troels Jessen <troels@space-inventor.com>
2025-11-06 14:17:21 +0100

    Client support for VMEM compression

--- src/slash_utils.c
+++ src/slash_utils.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <memory.h>
 #include <slash/slash.h>
 #include <apm/csh_api.h>
 
@@ -12,3 +13,39 @@ optparse_opt_t *csh_add_node_option(optparse_t * parser, unsigned int *node) {
 		get_host_by_addr_or_name, node);
 }
 
+int parse_vmem_address(struct slash *slash, int *argi, uint64_t *address) {
+
+	if (++(*argi) >= slash->argc) {
+		printf("missing address\n");
+		return SLASH_EUSAGE;
+	}
+
+	char * endptr;
+	*address = strtoul(slash->argv[*argi], &endptr, 16);
+	if (*endptr != '\0') {
+		printf("Failed to parse address\n");
+		return SLASH_EINVAL;
+	}
+
+	return SLASH_SUCCESS;
+}
+
+int parse_length(struct slash *slash, int *argi, uint32_t *length) {
+
+	if (++(*argi) >= slash->argc) {
+		printf("missing length\n");
+		return SLASH_EINVAL;
+	}
+
+	char * endptr;
+	*length = strtoul(slash->argv[*argi], &endptr, 10);
+	if (*endptr != '\0') {
+		*length = strtoul(slash->argv[*argi], &endptr, 16);
+		if (*endptr != '\0' || strncmp(slash->argv[*argi], "0x", 2) != 0) {
+			printf("Failed to parse length in base 10 or base 16\n");
+			return SLASH_EUSAGE;
+		}
+	}
+
+	return SLASH_SUCCESS;
+}
--- src/vmem_client_slash.c
+++ src/vmem_client_slash.c
@@ -17,6 +17,7 @@
 
 #include <vmem/vmem_client.h>
 #include <vmem/vmem_server.h>
+#include <vmem/vmem_codec.h>
 #include <csp/csp.h>
 #include <sys/types.h>
 
@@ -49,6 +50,110 @@ static int vmem_client_slash_list(struct slash *slash)
 
 }
 slash_command(vmem, vmem_client_slash_list, "", "List virtual memory");
+
+static int vmem_client_slash_decompress(struct slash *slash) {
+
+	int res = SLASH_SUCCESS;
+
+	unsigned int node = slash_dfl_node;
+	unsigned int timeout = slash_dfl_timeout;
+	unsigned int version = 1;
+
+	uint64_t src_address;
+	uint64_t dst_address;
+	uint32_t length;
+
+	optparse_t * parser = optparse_new("vmem decompress", "<src_address> <dst_address> [length base10 or base16]");
+	optparse_add_help(parser);
+	csh_add_node_option(parser, &node);
+	optparse_add_unsigned(parser, 't', "timeout", "NUM", 0, &timeout, "timeout (default = <env>)");
+	optparse_add_unsigned(parser, 'v', "version", "NUM", 0, &version, "version (default = 1)");
+
+	int argi = optparse_parse(parser, slash->argc - 1, (const char **) slash->argv + 1);
+	if (argi < 0) { 
+		res = SLASH_EINVAL;
+		goto failure;
+	}
+
+	res = parse_vmem_address(slash, &argi, &src_address);
+	if (res < 0) { goto failure; }
+
+	res = parse_vmem_address(slash, &argi, &dst_address);
+	if (res < 0) { goto failure; }
+
+	res = parse_length(slash, &argi, &length);
+	if (res < 0) { goto failure; }
+
+	res = vmem_client_decompress(node, timeout, src_address, dst_address, length, version);
+
+	if (res < 0) {
+		if (res == -1) {
+			res = SLASH_ENOMEM;
+		} else if (res == -2) {
+			printf("\033[31m\n");
+			printf("Timed out on codec response\n");
+			printf("\033[0m\n");
+			res = SLASH_EIO;
+		}
+	}
+
+failure:
+	optparse_del(parser);
+	return res;
+}
+slash_command_sub(vmem, decompress, vmem_client_slash_decompress, "<src_address> <dst_address> <length>", "Perform decompression from src into dst");
+
+static int vmem_client_slash_compress(struct slash *slash) {
+
+	int res = SLASH_SUCCESS;
+
+	unsigned int node = slash_dfl_node;
+	unsigned int timeout = slash_dfl_timeout;
+	unsigned int version = 1;
+
+	uint64_t src_address;
+	uint64_t dst_address;
+	uint32_t length;
+
+	optparse_t * parser = optparse_new("vmem compress", "<src_address> <dst_address> [length base10 or base16]");
+	optparse_add_help(parser);
+	csh_add_node_option(parser, &node);
+	optparse_add_unsigned(parser, 't', "timeout", "NUM", 0, &timeout, "timeout (default = <env>)");
+	optparse_add_unsigned(parser, 'v', "version", "NUM", 0, &version, "version (default = 1)");
+
+	int argi = optparse_parse(parser, slash->argc - 1, (const char **) slash->argv + 1);
+	if (argi < 0) { 
+		res = SLASH_EINVAL;
+		goto failure;
+	}
+
+	res = parse_vmem_address(slash, &argi, &src_address);
+	if (res < 0) { goto failure; }
+
+	res = parse_vmem_address(slash, &argi, &dst_address);
+	if (res < 0) { goto failure; }
+
+	res = parse_length(slash, &argi, &length);
+	if (res < 0) { goto failure; }
+
+	res = vmem_client_compress(node, timeout, src_address, dst_address, length, version);
+
+	if (res < 0) {
+		if (res == -1) {
+			res = SLASH_ENOMEM;
+		} else if (res == -2) {
+			printf("\033[31m\n");
+			printf("Timed out on codec response\n");
+			printf("\033[0m\n");
+			res = SLASH_EIO;
+		}
+	}
+
+failure:
+	optparse_del(parser);
+	return res;
+}
+slash_command_sub(vmem, compress, vmem_client_slash_compress, "<src_address> <dst_address> <length>", "Perform compression from src into dst");
 
 #if 0
 static int vmem_client_slash_fram(struct slash *slash, int backup) {
