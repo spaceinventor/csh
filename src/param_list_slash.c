c2c0455b4ace6847ad68d0f622e38960d47fade2
kevin <kevin.carlsen@space-inventor.com>
2025-11-12 11:00:52 +0100

    Revert "Added `param_completer()` to `list()` command"
    This reverts commit 913a5f8cafbb6458e242d98342512453958f8643.
    
    This commit broke tab-completion of `list` sub-commands, such as `list download`.

--- src/param_list_slash.c
+++ src/param_list_slash.c
@@ -62,7 +62,7 @@ Shows cached/known values. Use -v to include parameter type and help text.");
     optparse_del(parser);
     return SLASH_SUCCESS;
 }
-slash_command_completer(list, list, param_completer, "[OPTIONS...] [name wildcard=*]", "List parameters");
+slash_command(list, list, "[OPTIONS...] [name wildcard=*]", "List parameters");
 
 
 static int list_download(struct slash *slash)
