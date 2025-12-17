149b59985276821175d8ac7bd2e88dcf874934e4
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2025-12-17 20:57:45 +0100

    Update "hk_set_epoch" prototype to actually match the implementation

--- src/hk_param_sniffer.h
+++ src/hk_param_sniffer.h
@@ -12,7 +12,7 @@
 #include <csp/csp.h>
 
 bool hk_get_epoch(time_t* epoch, uint16_t node);
-void hk_set_epoch(time_t epoch, uint16_t node);
+void hk_set_epoch(time_t epoch, uint16_t node, bool auto_sync);
 /* returns true if the packet was found to be for housekeeping */
 bool hk_param_sniffer(csp_packet_t * packet);
 
