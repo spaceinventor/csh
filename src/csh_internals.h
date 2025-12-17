170d2227b5bc7a8bee4da578e4625c9b71f1d90f
Jean-Baptiste Lab <jean.baptiste@space-inventor.com>
2025-12-17 20:57:05 +0100

    Add more internal function prototypes

--- src/csh_internals.h
+++ src/csh_internals.h
@@ -9,6 +9,11 @@ extern "C" {
 void rdp_opt_add(optparse_t * parser);
 void rdp_opt_set(void);
 void rdp_opt_reset(void);
+void serial_init(void);
+uint32_t serial_get(void);
+void* si_lock_init(void);
+int si_lock_take(void* lock, int block_time_ms);
+int si_lock_give(void* lock);
 
 #ifdef __cplusplus
 }
