0cea220f2b1cb61adc026b6f7c56f30283e11605
Johan de Claville Christiansen <johan@space-inventor.com>
2026-02-13 19:48:37 +0100

    Fix inttypes

--- src/zmqproxy.c
+++ src/zmqproxy.c
@@ -20,6 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 #include <unistd.h>
 #include <stdlib.h>
+#include <inttypes.h>
 #include <zmq.h>
 #include <assert.h>
 #include <pthread.h>
