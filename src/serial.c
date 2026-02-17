5d549901a1c53907a22b03e81bc5f6d509035913
Johan de Claville Christiansen <johan@space-inventor.com>
2026-02-17 22:55:48 +0100

    submodule update
    * csp b4d75ee6...aceef30a (7):
      > csp_buffer: memcpy only what is used in the packet
      > meson option: change default to not zero csp packets
      > zmqhub: make iflist iterator public, return err in zmq funcs
      > inlined helper function
      > Remove packet copy when transmitting CSP packets
      > Remove packet copy when transmitting CSP packets (#942)
      < Merge pull request #33 from spaceinventor/router_avoid_packet_copy
    
    * ossi ba0ba6b...8a41b58 (1):
      > disable message_queue_create
    
    * param 523ab94...119c858 (4):
      > Set xint series as uint types
      > nice little comment to PARAM_TYPECHECK
      > Better param type check
      > use const in callback in param_collector

--- src/serial.c
+++ src/serial.c
@@ -9,7 +9,7 @@ uint32_t _serial0;
 
 #define PARAMID_SERIAL0                      31
 
-PARAM_DEFINE_STATIC_RAM(PARAMID_SERIAL0, serial0, PARAM_TYPE_INT32, -1, 0, PM_HWREG, NULL, "", &_serial0, NULL);
+PARAM_DEFINE_STATIC_RAM(PARAMID_SERIAL0, serial0, PARAM_TYPE_XINT32, -1, 0, PM_HWREG, NULL, "", &_serial0, NULL);
 
 void serial_init(void) {
     _serial0 = rand();
